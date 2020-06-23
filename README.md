b5d2fa14d0330c402fd75762afc603000a8f3196
ddeelskm <ddeelskm@gmail.com>
2020-06-23 11:04:39 +0400

    TheFormationOfTheLayout

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ All the changes you see in the commits.
 ---
 update 23.06.2020/
 - Output of the component template.
-
+- The formation of the layout.
 ---
 update 22.06.2020 /
 - Added layout for the project.
--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -1,8 +1,8 @@
 import {DomListener} from '@core/DomListener'
 
-export class ExcelComponent extends DomListener{
-
-    toHTML() {
-        return ''
-    }
+export class ExcelComponent extends DomListener {
+  // Возвращает шаблон компонента
+  toHTML() {
+    return ''
+  }
 }
