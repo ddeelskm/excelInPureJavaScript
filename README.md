e2617fddbb49d4266067ba3b2b5cef0166902dc6
ddeelskm <ddeelskm@gmail.com>
2020-06-23 10:09:57 +0400

    OutputOfTheComponentTemplate

--- README.md
+++ README.md
@@ -2,7 +2,11 @@ Excel
 ---
 All the changes you see in the commits.
 ---
+update 23.06.2020/
+- Output of the component template.
+
+---
 update 22.06.2020 /
-- Added layout for the project
-- Class structure
+- Added layout for the project.
+- Class structure.
 ---
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import {Excel} from '@/components/excel/Excel'
+import {Header} from '@/components/header/header'
+import {Toolbar} from '@/components/toolbar/Toolbar'
+import {Formula} from '@/components/formula/Formula'
+import {Table} from '@/components/table/Table'
 import './scss/index.scss'
 
 const excel = new Excel('#app', {
-    components: []
+    components: [Header, Toolbar, Formula, Table]
 })
